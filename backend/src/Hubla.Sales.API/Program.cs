7fb2d91ea1ca616e56f39c2d59e16083b3e43f8c
seygi.kutani <seygi@outlook.com>
2023-02-28 00:03:00 -0300

    default datas

---
+++ backend/src/Hubla.Sales.API/Program.cs
@@ -0,0 +1,52 @@
+using Hubla.Sales.API.Filters;
+using Hubla.Sales.Application;
+
+namespace Hubla.Sales.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers(options =>
+            {
+                options.Filters.Add<NotificationFilter>();
+            });
+            
+            /* inicio custom*/
+            ConfigurationManager configuration = builder.Configuration; // allows both to access and to set up the config
+            IWebHostEnvironment environment = builder.Environment;
+
+            builder.Services.AddApplication(configuration);
+            /* fim custom*/
+
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
