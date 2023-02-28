e39929ae85cd604b238facccf3a38d1205b167c1
seygi.kutani <seygi@outlook.com>
2023-02-28 00:02:28 -0300

    - controller inicial

---
+++ backend/src/Hubla.Sales.API/Controllers/SalesController.cs
@@ -0,0 +1,41 @@
+﻿using Hubla.Sales.Application.Features.CreateSale.UseCase;
+using Hubla.Sales.Application.Shared.UseCase;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Hubla.Sales.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SalesController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> CreateSaleAsync(IFormFile file,
+            [FromServices]
+            IUseCase<CreateSaleInput, CreateSaleOutput> useCase,
+            CancellationToken cancellationToken)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                await file.CopyToAsync(memoryStream);
+                var buffer = memoryStream.ToArray();
+                var input = CreateSaleInput.Create(buffer);
+
+                var output = await useCase.ExecuteAsync(input, cancellationToken);
+
+                return Ok();
+            }
+        }
+    }
+}
