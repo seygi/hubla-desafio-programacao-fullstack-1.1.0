7924876fcdbfa300c82c616e64fe35e0342ad423
seygi.kutani <seygi@outlook.com>
2023-02-27 23:54:25 -0300

    - create validator pattern

---
+++ backend/Hubla.Sales.Application/Shared/Validator/IValidatorService.cs
@@ -0,0 +1,12 @@
+﻿using Hubla.Sales.Application.Shared.Notifications;
+
+namespace Hubla.Sales.Application.Shared.Validator
+{
+    public interface IValidatorService<in TInput>
+        where TInput : class
+    {
+        bool ValidateAndNotifyIfError(TInput input);
+
+        bool Validate(TInput input, out NotificationErrors notificationErrors);
+    }
+}
---
+++ backend/Hubla.Sales.Application/Shared/Validator/ValidatorService.cs
@@ -0,0 +1,56 @@
+﻿using FluentValidation;
+using Hubla.Sales.Application.Shared.Notifications;
+using Microsoft.Extensions.Logging;
+using System.Net;
+
+namespace Hubla.Sales.Application.Shared.Validator
+{
+    public class ValidatorService<TInput> : IValidatorService<TInput>
+        where TInput : class
+    {
+        private readonly IValidator<TInput> _validator;
+        private readonly INotificationContext _notificationContext;
+        private readonly ILogger<ValidatorService<TInput>> _logger;
+
+        public ValidatorService(IValidator<TInput> validator, INotificationContext notificationContext, ILogger<ValidatorService<TInput>> logger) => (_validator, _notificationContext, _logger) = (validator, notificationContext, logger);
+
+        public bool ValidateAndNotifyIfError(TInput input)
+        {
+            var notificationErrors = NotificationErrors.Empty;
+            var result = _validator.Validate(input);
+
+            if (result.IsValid)
+                return result.IsValid;
+
+            foreach (var error in result.Errors)
+            {
+                notificationErrors.Add(error.PropertyName, error.ErrorMessage);
+            }
+
+            _logger.LogInformation("Input {Input} inválido", nameof(input));
+            _notificationContext.Create(HttpStatusCode.BadRequest, notificationErrors);
+
+            return result.IsValid;
+        }
+
+        public bool Validate(TInput input, out NotificationErrors notificationErrors)
+        {
+            notificationErrors = NotificationErrors.Empty;
+            var result = _validator.Validate(input);
+
+            if (!result.IsValid)
+            {
+                foreach (var error in result.Errors)
+                {
+                    notificationErrors.Add(error.PropertyName, error.ErrorMessage);
+                }
+
+                _logger.LogInformation("Input {Input} inválido", nameof(input));
+
+                _notificationContext.Create(HttpStatusCode.BadRequest, notificationErrors);
+            }
+
+            return result.IsValid;
+        }
+    }
+}
