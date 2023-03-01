cc95cf77490689fb8649148567cc07bc39738cb3
seygi.kutani <seygi@outlook.com>
2023-02-28 23:28:36 -0300

    - criado objetos base

---
+++ backend/Hubla.Sales.Application/Shared/Sales/UseCases/Outputs/SaleOutputBase.cs
@@ -0,0 +1,25 @@
+﻿using Hubla.Sales.Application.Shared.Sales.Enums;
+using Hubla.Sales.Application.Shared.Sellers.UseCases.Outputs;
+
+namespace Hubla.Sales.Application.Shared.Sales.UseCases.Outputs
+{
+    public abstract class SaleOutputBase
+    {
+        public int Id { get; set; }
+        public SaleType SaleType { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public double Value { get; set; }
+        public SellerOutputBase Seller { get; set; }
+
+        protected SaleOutputBase(int id, SaleType saleType, DateTime date, string description, double value, SellerOutputBase seller)
+        {
+            Id = id;
+            SaleType = saleType;
+            Date = date;
+            Description = description;
+            Value = value;
+            Seller = seller;
+        }
+    }
+}
---
+++ backend/Hubla.Sales.Application/Shared/Sellers/UseCases/Outputs/SellerOutputBase.cs
@@ -0,0 +1,14 @@
+﻿namespace Hubla.Sales.Application.Shared.Sellers.UseCases.Outputs
+{
+    public abstract class SellerOutputBase
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        protected SellerOutputBase(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
