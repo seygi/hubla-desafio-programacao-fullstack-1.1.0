e7cb770c8378d1e27ec6d9a4f193fed142538576
seygi.kutani <seygi@outlook.com>
2023-03-01 00:18:21 -0300

    - incluido property amounttotal

--- backend/src/Hubla.Sales.API/Transport/V1/GetSellers/GetSellerResponse.cs
+++ backend/src/Hubla.Sales.API/Transport/V1/GetSellers/GetSellerResponse.cs
@@ -6,15 +6,17 @@ namespace Hubla.Sales.API.Transport.V1.GetSellers
     public sealed class GetSellerResponse : SellerResponseBase
     {
         public IEnumerable<GetSellerSalesResponse> Sales { get; set; }
-        public GetSellerResponse(int id, string name, IEnumerable<GetSellerSalesResponse> sales) : base(id, name)
+        public decimal AmountTotal { get; set; }
+        public GetSellerResponse(int id, string name, IEnumerable<GetSellerSalesResponse> sales, decimal amountTotal) : base(id, name)
         {
             Sales = sales;
+            AmountTotal = amountTotal;
         }
 
         public static IList<GetSellerResponse> Create(GetSellersListOutput outputUseCase)
         {
             return outputUseCase
-                .Select(lnq => new GetSellerResponse(lnq.Id, lnq.Name, GetSellerSalesResponse.Create(lnq.Sales)))
+                .Select(lnq => new GetSellerResponse(lnq.Id, lnq.Name, GetSellerSalesResponse.Create(lnq.Sales), lnq.AmountTotal))
                .ToList();
         }
     }
