5fc8ca71341e3d861398e21e6d232fca9e6a675c
seygi.kutani <seygi@outlook.com>
2023-02-28 23:06:37 -0300

    - rename enums

--- backend/Hubla.Sales.Application/Shared/Sales/Enums/SaleType.cs
+++ backend/Hubla.Sales.Application/Shared/Sales/Enums/SaleType.cs
@@ -2,8 +2,8 @@ namespace Hubla.Sales.Application.Shared.Sales.Enums
 {
     public enum SaleType
     {
-        SaleProducer = 1,
-        SaleAffiliate = 2,
+        ProducerSale = 1,
+        AffiliateSale = 2,
         PaidComission = 3,
         ReceivedComission = 4
     }
